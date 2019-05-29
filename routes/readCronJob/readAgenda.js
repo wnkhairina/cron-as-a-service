f45e38d1feddb3cd1b1abb384ab4e9786562c072
wnkhairina <wnkhairina@gmail.com>
2019-05-29 09:41:39 +0800

    delete unused files

--- routes/readCronJob/readAgenda.js
+++
@@ -1,9 +0,0 @@
-const Job = require('../../schema/job');
-
-const readAgenda = async (req, res, job) => {
-    const jobs = await agenda.jobs({name: 'Ping website'}, function(err,jobs){
-        
-    });
-};
-
-module.exports = readAgenda
